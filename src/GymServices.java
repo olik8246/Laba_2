fc2a31dacbd6f6cc94c258230d45b5f0bed2270c
olik <olegkhomaa@gmail.com>
2023-10-26 20:04:52 +0300

    Initial commit

---
+++ src/GymServices.java
@@ -0,0 +1,10 @@
+class GymServices {
+    public void showServices() {
+        System.out.println("_______________________________________________");
+        System.out.println("Додаткові послуги в спортзалі:");
+        System.out.println("1. Персональний тренер");
+        System.out.println("2. Сауна");
+        System.out.println("3. Масаж");
+        System.out.println("_______________________________________________");
+    }
+}
